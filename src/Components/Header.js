8375f1efaa59afcfaffab9425bf6b74abede9496
B6122683 <64888010+B6122683@users.noreply.github.com>
2022-05-18 18:41:22 +0700

    add

---
+++ src/Components/Header.js
@@ -0,0 +1,49 @@
+import * as React from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  Button,
+  Tooltip,
+  MenuItem,
+  Badge,
+} from "@mui/material";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import logo1 from "../Images/ics.png";
+import avatar1 from "../Images/avatar.png";
+
+const Header = () => {
+  return (
+    <AppBar position="sticky" style={{backgroundColor: "#134B8A"}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters style={{ justifyContent: "space-between" }}>
+            <Box sx={{ flexGrow: 0 }}>
+              <img
+                src={logo1}
+                alt=""
+                style={{ width: "45px", height: "45px" }}
+              />
+            </Box>
+            <Box
+              sx={{ flexGrow: 0 }}
+              mx={2}
+            >
+              {/* <Badge color="error" variant="dot">
+                <NotificationsIcon />
+              </Badge> */}
+              <IconButton sx={{ p: 0 }}>
+                <Avatar alt="Akkrapol" src={avatar1} />
+              </IconButton>
+
+            </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Header;
---
+++ src/Components/PlaceSearch.js
@@ -0,0 +1,164 @@
+import * as React from "react";
+import {
+  Container,
+  Box,
+  MenuItem,
+  FormControl,
+  Select,
+  TextField,
+  Grid,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+} from "@mui/material";
+import data from "../Data/example_data.json";
+
+export default function BasicSelect() {
+  const [types, setTypes] = React.useState("");
+  const [name, setName] = React.useState("");
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleChange = (event) => {
+    setTypes(event.target.value);
+  };
+
+  return (
+    <>
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h5" component="div">
+                Place List
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={types}
+              onChange={handleChange}
+              style={{ width: "100%" }}
+            >
+              <MenuItem value="restaurant">Restaurant</MenuItem>
+              <MenuItem value="bakery">Bakery</MenuItem>
+              <MenuItem value="cafe">Cafe</MenuItem>
+            </Select>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <TextField
+              style={{ width: "100%" }}
+              id="outlined-name"
+              value={name}
+              onChange={handleChangeName}
+              placeholder="Search restaurant..."
+            />
+          </Grid>
+        </Grid>
+      </Container>
+      <Container sx={{ py: 2 }} maxWidth="lg">
+        <Grid container spacing={2}>
+          {data
+            .filter((item) => {
+              if (name === "") {
+                return item;
+              } else {
+                return item.name.toLowerCase().includes(name.toLowerCase());
+              }
+            })
+            .map((item) => (
+              <Grid item key={item.id} xs={12} sm={4} md={4}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardHeader
+                    avatar={
+                      <img
+                        src={item.profile_image_url}
+                        alt={item.name}
+                        style={{
+                          width: "50px",
+                          height: "50px",
+                          borderRadius: "10px",
+                        }}
+                      />
+                    }
+                    title={
+                      <Typography variant="h6" component="div">
+                        {item.name}
+                      </Typography>
+                    }
+                    subheader={
+                      <Typography component="div">{item.rating}</Typography>
+                    }
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Box
+                      sx={{
+                        width: "100%",
+                        display: { xs: "none", md: "flex" },
+                      }}
+                      mt={2}
+                    >
+                      <Grid container>
+                        <Grid item xs={12} md={4}>
+                          <CardMedia
+                            component="img"
+                            style={{ height: "120px", width: "120px" }}
+                            image={item.images[0]}
+                            alt={item.name}
+                          />
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                          <CardMedia
+                            component="img"
+                            style={{ height: "120px", width: "120px" }}
+                            image={item.images[1]}
+                            alt={item.name}
+                          />
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                          <CardMedia
+                            component="img"
+                            style={{ height: "120px", width: "120px" }}
+                            image={item.images[2]}
+                            alt={item.name}
+                          />
+                        </Grid>
+                      </Grid>
+                    </Box>
+                    <Box
+                      sx={{
+                        width: "100%",
+                        display: { xs: "flex", md: "none" },
+                      }}
+                    >
+                      <Grid container>
+                        <Grid item xs={12}>
+                          <CardMedia
+                            component="img"
+                            style={{ height: "60px" }}
+                            image={item.images[0]}
+                            alt={item.name}
+                          />
+                        </Grid>
+                      </Grid>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+        </Grid>
+      </Container>
+    </>
+  );
+}
